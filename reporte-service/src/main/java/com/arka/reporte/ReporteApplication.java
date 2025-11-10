902c78a36ffa97a0912c7a001e079f7dfcff3c50
Erick_jmp <pkemon025@gmail.com>
2025-11-10 15:10:32 -0600

    subiendo imagen a ec2

--- reporte-service/src/main/java/com/arka/reporte/ReporteApplication.java
+++ reporte-service/src/main/java/com/arka/reporte/ReporteApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class ReporteApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(ReporteApplication.class, args);
 	}
