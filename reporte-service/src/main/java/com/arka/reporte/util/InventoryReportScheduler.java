2be177f811a8b607d63489dcf7fa125365cdd12b
Armando Mejia <armandoa.mejiad@outlook.com>
2025-11-10 13:37:57 -0600

    Agregando envio csv a s3

--- reporte-service/src/main/java/com/arka/reporte/util/InventoryReportScheduler.java
+++ reporte-service/src/main/java/com/arka/reporte/util/InventoryReportScheduler.java
@@ -2,8 +2,12 @@ package com.arka.reporte.util;
 
 import com.arka.reporte.service.InventoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -17,21 +21,30 @@ public class InventoryReportScheduler {
 
     private final InventoryService inventoryService;
     private final CsvExporter csvExporter;
+    private final S3Client s3Client;
+    @Value("${cloud.aws.s3.bucket.name}")
+    private String bucketName;
 
     @Scheduled(fixedRateString = "#{@reportProperties.interval}")
+    // Ejecuta cada 1 días (configurable con reportProperties.weeklyCron)
+    //@Scheduled(cron = "${reportProperties.weeklyCron}")
     public void generateCsvReport() {
         inventoryService.getLowStockItems()
                 .collectList()
                 .map(csvExporter::generateInventoryCsv)
                 .subscribe(csv -> {
+                    String key = "low_stock_inventory.csv";
                     // Aquí se puede guardar el archivo en disco
-                    Path path = Paths.get("low_stock_inventory.csv");
-                    try {
-                        Files.write(path, csv.getBytes(StandardCharsets.UTF_8));
-                        System.out.println("Reporte generado: " + path.toAbsolutePath());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    //Path path = Paths.get("low_stock_inventory.csv");
+                    s3Client.putObject(PutObjectRequest.builder()
+                                    .bucket(bucketName)
+                                    .key(key)
+                                    .contentType("text/csv")
+                                    .build(),
+                    RequestBody.fromBytes(csv.getBytes(StandardCharsets.UTF_8)));
+                    System.out.println("Reporte subido a S3: s3://" + bucketName + "/" + key);
+                    //Files.write(path, csv.getBytes(StandardCharsets.UTF_8));
+                    //System.out.println("Reporte generado: " + path.toAbsolutePath());
                 });
     }
 
